bbbf230618d04878744b406deba0bf3e8610a6a0
Marieke Hutsebaut <marieke@tinseltown.be>
2021-09-10 18:34:16 +0200

    added project to git

---
+++ app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
@@ -0,0 +1,35 @@
+package com.hfad.starbuzz;
+
+import android.app.*;
+import android.content.*;
+import android.view.*;
+import android.widget.*;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.*;
+
+public class DrinkCategoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_category);
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1,
+                Drink.drinks);
+        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
+        listDrinks.setAdapter(listAdapter);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
+                Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
+                startActivity(intent);
+
+            }
+        };
+        listDrinks.setOnItemClickListener(itemClickListener);
+    }
+}
