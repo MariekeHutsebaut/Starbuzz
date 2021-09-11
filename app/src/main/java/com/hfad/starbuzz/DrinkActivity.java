99f5aa7b6596a1d00b4bd940e0bc6610b2ce2c56
Marieke Hutsebaut <marieke@tinseltown.be>
2021-09-11 09:28:07 +0200

    added DrinkActivity

--- app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
+++ app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
@@ -1,13 +1,30 @@
 package com.hfad.starbuzz;
 
+import android.app.*;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import org.w3c.dom.*;
 
-public class DrinkActivity extends AppCompatActivity {
+public class DrinkActivity extends Activity {
+    public static final String EXTRA_DRINKID = "drinkId";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+
+        int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINKID);
+        Drink drink = Drink.drinks[drinkId];
+
+        TextView name = (TextView) findViewById(R.id.name);
+        name.setText(drink.getName());
+
+        TextView description = (TextView) findViewById(R.id.description);
+        description.setText(drink.getDescription());
+
+        ImageView photo = (ImageView) findViewById(R.id.photo);
+        photo.setImageResource(drink.getImageResourceId());
+        photo.setContentDescription(drink.getName());
     }
 }
